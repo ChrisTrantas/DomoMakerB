844226fe6dffb711a82725967429261af1afad63
Christopher Trantas (RIT Student) <cmt4536@ad.rit.edu>
2016-03-29 11:09:55 -0400

    Fixed a error

--- src/controllers/Domo.js
+++ src/controllers/Domo.js
@@ -17,11 +17,13 @@ var makerPage = function(req, res){
 var makeDomo = function(req, res){
 	
 	if(!req.body.name || !req.body.age){
-		
+		return res.status(400).json({error: "RAWR! Both name and age are required"});
 	}
 	
 	var domoData = {
-		
+		name: req.body.name,
+		age: req.body.age,
+		owner: req.session.account._id
 	};
 	
 	var newDomo = new Domo.DOmoModel(domoData);
